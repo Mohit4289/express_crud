833620b67f5e0326e5d78b814dbe0cdbffded401
MOHIT Tiwari <168074292+Mohit4289@users.noreply.github.com>
2024-10-20 14:41:14 +0530

    Update user.route.js
    added login route

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,10 +1,11 @@
 import Router from "express"
-import {users, signup, update, deleteuser} from '../controller/user.control.js'
+import {users, signup, update, login, deleteuser} from '../controller/user.control.js'
 
 const userRouter = Router()
 
 userRouter.get("/users", users)
 userRouter.post("/signup", signup )
+userRouter.post("/login", login)
 userRouter.put("/update/:id", update)
 userRouter.delete("/delete/:id", deleteuser)
 
